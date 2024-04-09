06421f7a328798a56796719d830a669e6c0103fa
Marko <msrpak001@gmail.com>
2024-04-09 10:57:52 +0200

    Working screen

--- components/gui/gui_app.c
+++ components/gui/gui_app.c
@@ -14,17 +14,15 @@
 /* Littlevgl specific */
 #include "lvgl.h"
 #include "lvgl_helpers.h"
+
+// squareline
+#include "ui.h"
+
 //---------------------------------- MACROS -----------------------------------
 
 //-------------------------------- DATA TYPES ---------------------------------
 
 //---------------------- PRIVATE FUNCTION PROTOTYPES --------------------------
-/**
- * @brief This function handles events that happen on LVGL objects.
- *
- * @param [in] p_event Pointer to the event type.
- */
-static void _button_1_event_handler(lv_event_t *p_event);
 
 //------------------------- STATIC DATA & CONSTANTS ---------------------------
 
@@ -33,26 +31,9 @@ static void _button_1_event_handler(lv_event_t *p_event);
 //------------------------------ PUBLIC FUNCTIONS -----------------------------
 void gui_app_init(void)
 {
-    /* Create buttons */
-    lv_obj_t *p_label1;
-    lv_obj_t *p_btn1 = lv_btn_create(lv_scr_act());
-    lv_obj_align_to(p_btn1, NULL, LV_ALIGN_CENTER, 0, -80);
-    p_label1 = lv_label_create(p_btn1);
-    lv_label_set_text(p_label1, "Button 1");
-
-    // Add button 1 callback
-    (void)lv_obj_add_event_cb(p_btn1, _button_1_event_handler, LV_EVENT_CLICKED,
-                              NULL);
+    ui_init();
 }
 
 //---------------------------- PRIVATE FUNCTIONS ------------------------------
-static void _button_1_event_handler(lv_event_t *p_event)
-{
-    if (LV_EVENT_CLICKED == p_event->code) {
-        printf("Button 1 clicked\n");
-    } else {
-        /* Do nothing.*/
-    }
-}
 
 //---------------------------- INTERRUPT HANDLERS -----------------------------
